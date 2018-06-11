1801d3d64331c21346858c9dab83894bf8382a20
klynx <KevinLynx_L@hotmail.com>
2018-06-11 13:48:01 -0700

    Added request with hidden key

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
 var request = require('request');
-
+var secret = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+   
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secret.GITHUB_TOKEN
+    }
+  };
+
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  }); 
+
+
+
+request(options, function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+}
